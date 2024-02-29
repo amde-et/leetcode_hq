59341d551f6baa144f7f0edba57d66897faa96f4
Amdebirhan Abebe <99601438+amde-et@users.noreply.github.com>
2024-02-29 12:01:13 +0300

    Add solution for Same Tree

---
+++ leetcode/same-tree.py
@@ -0,0 +1,4 @@
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        return str(p)==str(q)
+        #i swear this will be the finniest code i ever write if it works
